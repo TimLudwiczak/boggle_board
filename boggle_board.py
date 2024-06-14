7ccb8d4d68d37716eb0c353d11145aaae0f7f4de
Tim Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-13 20:11:12 -0500

    Random Generator added

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,13 @@
+import random
+
 class BoggleBoard:
   
-  def __init__(self):
+  def __init__(self, size = 4 ):
+    self.size = size
+    self.board = self.rand_board()
+  
+  def rand_board(self):
+    board = []
   
 
   def shake(self):
